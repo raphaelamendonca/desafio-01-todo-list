ecb9e98dd6ff6b7c626f7d414e10277562eaa84c
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2023-03-02 19:16:17 -0300

    feat: Cria função handleSubmit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,10 +2,16 @@ import { Button } from "./Button";
 import { Input } from "./Input";
 
 import styles from "./Form.module.css";
+import { ChangeEvent, FormEvent } from "react";
+
+function handleSubmit(event: FormEvent) {
+  // evita que a página seja recarregada
+  event.preventDefault();
+}
 
 export function Form() {
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <Input />
       <Button />
     </form>
