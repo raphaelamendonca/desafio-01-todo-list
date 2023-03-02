da1de0bbf9f0cffab783a327566161cd5e1d0dc7
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2023-03-02 19:13:21 -0300

    feat: Cria função handleNewTaskChange

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,17 @@
+import { ChangeEvent, useState } from "react";
 import styles from "./Input.module.css";
 
 export function Input() {
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.value);
+  }
+
   return (
     <input
       type="text"
       placeholder="Adicione uma nova tarefa"
       className={styles.input}
+      onChange={handleNewTaskChange}
     ></input>
   );
 }
